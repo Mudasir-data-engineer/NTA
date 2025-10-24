33d7f30d90666de813e52540f28fd42684890891
Mudasir-data-engineer <mudasirshafiq007@hotmail.com>
2025-10-24 21:48:30 +0200

    frontend updated

---
+++ backend/src/main/java/com/example/nta/NTAApplication.java
@@ -0,0 +1,13 @@
+package com.example.nta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NTAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NTAApplication.class, args);
+    }
+
+}
